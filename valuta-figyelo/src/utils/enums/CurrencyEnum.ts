1bb84116fe8f6e8948b005ae4511e6523c6c565f
KncskBenedek <koncsikbenedek@gmail.com>
2025-05-08 16:11:16 +0200

    enumok, interfacek

---
+++ valuta-figyelo/src/utils/enums/CurrencyEnum.ts
@@ -0,0 +1,17 @@
+export enum CurrencyEnum {
+    GBP = "gbp",
+    AUD = "aud",
+    DKK = "dkk",
+    JPY = "jpy",
+    CAD = "cad",
+    NOK = "nok",
+    CHF = "chf",
+    SEK = "sek",
+    USD = "usd",
+    CZK = "czk",
+    PLN = "pln",
+    EUR = "eur",
+    HRK = "hrk",
+    RON = "ron",
+    TRY = "try"
+}
