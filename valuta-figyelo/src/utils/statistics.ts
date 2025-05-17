24930defff24b24af10e1b1bfb9f5a3726be8aff
KncskBenedek <koncsikbenedek@gmail.com>
2025-05-17 21:37:29 +0200

    alap fügvények

---
+++ valuta-figyelo/src/utils/statistics.ts
@@ -0,0 +1,17 @@
+//átlag tapasztalati várható érték
+export const mean = (data)=> {
+
+}
+//korrigált tapasztalati szórás
+export const sd = (data)=>{
+
+}
+//tapasztalati kovariancia
+export const cov = (data)=>{
+
+}
+
+//korreláció
+export const cor = (data)=>{
+
+}
