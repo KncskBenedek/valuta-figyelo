e5a1d6aeb97af7fb175a54b7b0797797e4edcbb1
KncskBenedek <koncsikbenedek@gmail.com>
2025-05-17 21:48:20 +0200

    sd

--- valuta-figyelo/src/utils/statistics.ts
+++ valuta-figyelo/src/utils/statistics.ts
@@ -10,7 +10,15 @@ export const mean = (data : number[])=> {
 }
 //korrigált tapasztalati szórás
 export const sd = (data: number[])=>{
-
+    const meanX = mean(data) 
+    const N = data.length;
+    if (N <= 0) return 0
+    let sum = data[0]
+    for (let i = 1; i < N; i++) {
+        sum += ((data[i] - meanX)**2)
+    }
+    const szorzo = 1/(Math.sqrt(N-1))
+    return (szorzo * Math.sqrt(sum))
 }
 //tapasztalati kovariancia
 export const cov = (data: number[])=>{
