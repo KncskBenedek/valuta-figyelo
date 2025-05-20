6a2b25c8e5b1c1aa0f61f3191dd2f5e70d113459
KncskBenedek <koncsikbenedek@gmail.com>
2025-05-20 23:43:05 +0200

    footer nincs össze nyomva

--- valuta-figyelo/src/components/Footer.tsx
+++ valuta-figyelo/src/components/Footer.tsx
@@ -1,11 +1,9 @@
 export default function Footer() {
   return (
-    <div className="container container-fluid">
-      <footer className="navbar bg-body-tertiary bottom">
-        <div className="container-fluid">
-          <p>@Copyright Minden jog fenntartva</p>
-        </div>
-      </footer>
-    </div>
+    <footer className="navbar bg-body-tertiary bottom">
+      <div className="container-fluid">
+        <p>@Copyright Minden jog fenntartva</p>
+      </div>
+    </footer>
   );
 }
