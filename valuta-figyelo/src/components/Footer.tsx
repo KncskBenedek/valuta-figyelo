777ce86d2b24a4bfebf2352e4bc50a442c46108e
KncskBenedek <koncsikbenedek@gmail.com>
2025-05-20 23:52:03 +0200

    adat hivatkozás

--- valuta-figyelo/src/components/Footer.tsx
+++ valuta-figyelo/src/components/Footer.tsx
@@ -1,8 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="navbar bg-body-tertiary bottom">
+    <footer className="navbar bg-body-tertiary">
       <div className="container-fluid">
         <p>@Copyright Minden jog fenntartva</p>
+        <a href="https://www.napiarfolyam.hu/">
+          Az árfolyamokat a napiárfolyam.hu gyüjti
+        </a>
+        <p>Készítette: Koncsik Benedek</p>
       </div>
     </footer>
   );
